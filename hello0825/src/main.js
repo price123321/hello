229c3dde6861828143d482bbe6487d08c0fbe85e
price123321 <1254175024@qq.com>
2025-08-30 13:51:57 +0800

    when i use the single file mode, i can run the demo;but i can't run the demo using html mode. it's soooo strange.and i decide to ignore this thing.

--- hello0825/src/main.js
+++ hello0825/src/main.js
@@ -1,20 +1,6 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import './assets/main.css'
+
 createApp(App).mount('#app')
-
-
-// const app = createApp({
-//   data() {
-//     return {
-//       count: 0
-//     }
-
-// //     el:"#app",
-// //     message: "hello vue"
-//   }
-// })
-
-// app.mount('#app1')
---
+++ hello0825/webpack..config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // ... other webpack configurations
+  resolve: {
+    alias: {
+      'vue': 'vue/dist/vue.esm-bundler.js'
+    }
+  }
+};
