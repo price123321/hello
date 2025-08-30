86982829af0ffb2472a2ef718669a454c26a0f82
price123321 <1254175024@qq.com>
2025-08-30 12:23:20 +0800

    try calc function. but i still don't understand vue run logic, for example i don't kown how to transport the origin js code to vue.

--- hello0825/src/App.vue
+++ hello0825/src/App.vue
@@ -1,19 +1,21 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import myCalcTest from './components/myCalcTest.vue';
 </script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
+    <!-- <div class="wrapper">
       <HelloWorld msg="You did it!" />
-    </div>
+    </div> -->
   </header>
 
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
+     <myCalcTest />
   </main>
 </template>
 
---
+++ hello0825/src/components/myCalcTest.vue
@@ -0,0 +1,53 @@
+<script setup>
+
+import { ref, onMounted } from 'vue';
+const eInput= ref(null);
+const piInput= ref(null);
+const eValue =  ref(null);
+const piValue = ref(null);
+
+ onMounted(() => {
+    //   console.log(myElement.value); // Access the DOM element
+    //   myElement.value.textContent = "New content!";
+     console.log(eInput);
+    console.log(eInput.value);
+
+this.$refs.e-slider-ref.addEventListener("input", function () {
+  e.style.transform = `rotate(calc(1deg * pow(${this.value}, e)))`;
+  eValue.textContent = e.style.transform;
+});
+
+piInput.addEventListener("input", function () {
+  pi.style.rotate = `calc(sin(${this.value} * pi) * 100deg)`;
+  piValue.textContent = pi.style.rotate;
+});
+
+    });
+
+// sliders
+// const eInput = document.querySelector("#e-slider");
+// const piInput = document.querySelector("#pi-slider");
+// spans for displaying values
+// const eValue = document.querySelector("#e-value");
+// const piValue = document.querySelector("#pi-value");
+
+
+</script>
+
+<template>
+<div id="wrapper">
+  <div class="container">
+    <div id="e"></div>
+    <input ref="e-slider-ref" type="range" min="0" max="7" step="0.01" value="0" id="e-slider" />
+    <label for="e-slider">e:</label>
+    <span id="e-value"></span>
+  </div>
+  <div class="container">
+    <div id="pi"></div>
+    <input type="range" min="0" max="1" step="0.01" value="0" id="pi-slider" />
+    <label for="pi-slider">pi:</label>
+    <span id="pi-value"></span>
+  </div>
+</div>
+</template>
+
